499506169d6ba2172fcd69a19df9cb638e7c91fe
Colin Poon <me@colinpoon.com>
2021-10-11 18:32:44 -0700

    cohort > 18

---
+++ 3_group_by/4_large_cohort.sql
@@ -0,0 +1,10 @@
+-- SELECT cohorts.name as cohort_name, COUNT(students*) as student_count
+-- FROM cohorts
+-- GROUP BY cohort_name
+-- ORDER BY student_count ASC;
+SELECT cohorts.name as cohort_name, count(students.*) AS student_count 
+FROM cohorts
+JOIN students ON cohorts.id = cohort_id
+GROUP BY cohort_name 
+HAVING count(students.*) > 17
+ORDER BY student_count;
