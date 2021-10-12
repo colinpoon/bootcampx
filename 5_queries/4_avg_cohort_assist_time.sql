35270f894b1111554f13bc094c97ce2ae8e6c08e
Colin Poon <me@colinpoon.com>
2021-10-12 16:05:30 -0700

    questions switched. fixed

--- 5_queries/4_avg_cohort_assist_time.sql
+++ 5_queries/4_avg_cohort_assist_time.sql
@@ -1,8 +1,6 @@
 SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
 FROM assistance_requests 
 JOIN students ON students.id = assistance_requests.student_id
--- combine rows from two+ based on a related column between them.
 JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY cohorts.name
-ORDER BY average_assistance_time DESC
-LIMIT 1;
+ORDER BY average_assistance_time;
