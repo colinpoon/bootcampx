5276e19e3742c62ff60c9df74d9a6161d30d17f9
Colin Poon <me@colinpoon.com>
2021-10-11 17:32:41 -0700

    0_join logic

---
+++ 2_join/0_join.sql
@@ -0,0 +1,5 @@
+SELECT students.name as student_name, email, cohorts.name as cohort_name
+FROM cohorts JOIN students ON cohort_id = cohorts.id;
+
+-- SELECT students.name as student_name, email, cohorts.name as cohort_name
+-- FROM students JOIN cohorts ON cohorts.id = cohort_id;
