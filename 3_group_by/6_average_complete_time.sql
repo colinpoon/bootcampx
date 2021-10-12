63bdc0768a498d7c5c2601da72c9e786a9828a23
Colin Poon <me@colinpoon.com>
2021-10-11 18:52:43 -0700

    Q6

---
+++ 3_group_by/6_average_complete_time.sql
@@ -0,0 +1,13 @@
+-- SELECT students.name as student_name, AVG(assignment_submissions.duration) as average_assignment_duration
+-- FROM students 
+-- JOIN assignment_submissions ON student_id = students.id
+-- WHERE end_date IS NULL
+-- GROUP BY student
+-- ORDER BY average_assignment_duration DESC;
+
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+WHERE end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
