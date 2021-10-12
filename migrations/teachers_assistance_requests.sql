ab9d32e6f3e8781622ecc75e0d4f9ba830822272
Colin Poon <me@colinpoon.com>
2021-10-12 15:58:04 -0700

    AVG assist time

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -1,3 +1,6 @@
+-- DROP TABLE if exists assistance_requests CASCADE;
+-- DROP TABLE if exists teachers CASCADE;
+
 CREATE TABLE teachers (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
@@ -17,23 +20,3 @@ CREATE TABLE assistance_requests (
   student_feedback TEXT,
   teacher_feedback TEXT
 );
-
-
-
-
--- CREATE TABLE assignments (
---   id SERIAL PRIMARY KEY NOT NULL,
---   name VARCHAR(255),
---   content TEXT,
---   day INTEGER,
---   chapter INTEGER,
---   duration INTEGER
--- );
-
--- CREATE TABLE assignment_submissions (
---   id SERIAL PRIMARY KEY NOT NULL,
---   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
---   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
---   duration INTEGER,
---   submission_date DATE
--- );
