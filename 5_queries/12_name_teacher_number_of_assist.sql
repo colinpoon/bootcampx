c63bb78b13eb474ab52e731c1dc6173e5d026be3
Colin Poon <me@colinpoon.com>
2021-10-12 17:11:33 -0700

    q12

---
+++ 5_queries/12_name_teacher_number_of_assist.sql
@@ -0,0 +1,27 @@
+SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
+
+-- SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.*) as total_assistances
+-- FROM teachers
+-- JOIN assistance_requests ON teacher_id = teachers.id
+-- JOIN students ON student_id = students.id
+-- JOIN cohorts ON cohort_id = cohorts.id
+-- WHERE cohorts.name = 'JULY02'
+-- GROUP BY teachers.name, cohorts.name
+-- ORDER BY teacher;
+
+
+
+-- SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, 
+-- FROM assistance_requests
+-- JOIN assistance_requests ON teacher_id = teachers.id
+-- JOIN students ON student_id = students.id
+-- JOIN cohorts ON cohort_id = cohorts.id
+-- WHERE cohorts.name = 'JUL02'
+-- ORDER BY teacher.name;
