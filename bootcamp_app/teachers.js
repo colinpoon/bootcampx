069980eee07fbd6469f80322b82f2a48c5d24b79
colinpoon <me@colinpoon.com>
2021-10-26 01:51:53 +0000

    mysql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,3 +1,4 @@
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -6,34 +7,26 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+
 const queryString = `
-  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-  FROM students
-  JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE $1
-  LIMIT $2;
-  `;
-
-  const cohortName = process.argv[2];
-  const limit = process.argv[3] || 5;
-  // Store all potentially malicious values in an array.
-  const values = [`%${cohortName}%`, limit];
-
-
-pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+`;
+
+const limit = [ process.argv[2] || 'JUL02'];
+
+pool.query(queryString, limit)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`)
+    })
   })
-});
+  .catch(err => console.error('query error', err.stack));
 
 
 // const cohort = process.argv[2];
